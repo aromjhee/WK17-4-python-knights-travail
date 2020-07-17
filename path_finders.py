9b4082883876bec5691b65f143228b101c2e9ade
Arom Jhee <aromjhee@gmail.com>
2020-07-17 09:13:20 -0700

    finish proj

--- path_finders.py
+++ path_finders.py
@@ -30,16 +30,40 @@ class KnightPathFinder:
     def build_move_tree(self):
         tree = [self._root]
 
-        while tree:
+        while len(tree) > 0:
             node = tree.pop(0)
             moves = self.new_move_positions(node.value)
             for move in moves:
-                node.add_child(Node(move))
-                tree.append(Node(move))
+                possible = Node(move)
+                node.add_child(possible)
+                tree.append(possible)
+    
+    def find_path(self, end_position):
+        node = self._root.breadth_search(end_position)
+        list = [pos.value for pos in self.trace_to_root(node)]
+        return list
+    
+    def trace_to_root(self, end_node):
+        list = []
+        current = end_node
+        while current != None:
+            list.append(current)
+            if current.parent != None:
+                current = current.parent
+            else:
+                current = None
+        return list
 
 
+
+# finder = KnightPathFinder((0, 0))
+# finder.build_move_tree()
+# print(finder._root.children)   # [<tree.Node object at 0x108fc6520>, <tree.Node object at 0x108fc6850>]
 
 finder = KnightPathFinder((0, 0))
 finder.build_move_tree()
-print(finder._root.children)
-# print(finder.new_move_positions((0, 0)))
+print(finder.find_path((2, 1)))  # => [(0, 0), (2, 1)]
+print(finder.find_path((3, 3)))  # => [(0, 0), (2, 1), (3, 3)]
+print(finder.find_path((6, 2)))  # => [(0, 0), (1, 2), (2, 4), (4, 3), (6, 2)]
+# => [(0, 0), (1, 2), (2, 4), (4, 3), (5, 5), (7, 6)]
+print(finder.find_path((7, 6)))
